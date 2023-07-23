9b3242635af8d312cd3fe054e89befa81aefb7f6
yoo-hyunseung <hsung122@naver.com>
2023-07-23 17:18:38 +0900

    reactStart

---
+++ src/hello.js
@@ -0,0 +1,10 @@
+import React from "react";
+// react 컴포넌트를 만들때 작성해야한다.
+// 리액트 컴포넌트는 함수형태, 클래스형태 둘다 작성할 수 있다.
+// 
+function Hello(){
+  return <div>hello react</div>  // jsx -> 자바 스크립트
+}
+
+export default Hello;
+// Hello 컴포넌트를 내보낸다는 뜻
--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './index.css';  // html jsp -> style
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Hello from './hello';
+const col = "{color:red}"
+const name = {firstname : 'hello',lastname : "React"};
+function formatUser(name){
+  return name.firstname + ' ' + name.lastname;
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <>
+  <App/>
+  <hr/>
+  <br/>
+  <Hello/>
+  {alert("hello react")}
+  </>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
